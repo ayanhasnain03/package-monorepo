dab3952ab38e7fdd57e2ca906e9180f561e0c6b5
Ayan <ayan2572006@gmail.com>
2024-09-06 03:21:27 +0530

    package done

--- server/index.ts
+++ server/index.ts
@@ -16,11 +16,4 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-mongoose
-  .connect("mongodb://localhost:27017/courses", { dbName: "courses" })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect("mongodb://localhost:27017/courses", { dbName: "courses" });
